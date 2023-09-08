6cee3faa1b99779bb33c20ade4d4fdc2ea654f1d
balajikosuri <balajikosuri2@gmail.com>
2023-09-08 18:19:37 +0000

    final commit

---
+++ src/components/MyFooter/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const MyFooter = () => (
+  <footer className="my-footer">
+    <p>
+      WebApp built by{' '}
+      <a
+        href="https://www.linkedin.com/in/balajikosuri"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <b> Balaji Kosuri </b>
+      </a>
+      <span>Visit My LinkedIn Profile </span>
+      <a
+        href="https://www.linkedin.com/in/balajikosuri"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <b> Click Here</b>
+      </a>
+    </p>
+  </footer>
+)
+
+export default MyFooter
